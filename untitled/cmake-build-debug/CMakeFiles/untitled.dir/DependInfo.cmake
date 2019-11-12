81321ab89f46113c77cd8c590adb95b870e7560d
roey-magen <57194211+roey-magen@users.noreply.github.com>
2019-11-12 14:49:30 +0200

    add roey amazing

--- untitled/cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
+++ untitled/cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/roeymagen/CLionProjects/untitled/main.cpp" "/Users/roeymagen/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
+  "/Users/roeymagen/CLionProjects/projectA/untitled/main.cpp" "/Users/roeymagen/CLionProjects/projectA/untitled/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,5 +2,6 @@
 
 int main() {
     std::cout << "Hello, World you are amazing!" << std::endl;
+    std::cout<<"roey you are amazing"<<std::endl;
     return 0;
 }
